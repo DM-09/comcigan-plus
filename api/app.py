5ee30991d700b3e4f4407fedca0e11709801a5a6
dongmin <happydm09@naver.com>
2024-08-15 19:02:40 +0900

    Create app.py

---
+++ api/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, render_template, url_for
+from flask_cors import CORS
+from comcigan_parser import *
+from datetime import *
+
+app = Flask(__name__)
+CORS(app)
+table = TimeTable('학교')
+start = datetime(*map(int, table.start_date.split('-')))
+data = table.data
+
+@app.route('/getTable/<string:school>')
+def getTable(school):
+  tt = TimeTable(school)
+  return jsonify({"update_date" : tt.update_date, "school_code" : tt.school_code,
+                  "region" : tt.region, 'Timetable_st' : tt.data, 'Today_num' : tt.today_num,
+                  "date_data" : tt.date_data, "start_date" : tt.start_date, "class_time" : tt.class_time,
+                  "Timetable_th" : tt.th_data, 'Teachers' : tt.tdata})
+
+@app.route('/searchSchool/<string:query>')
+def search(query):
+  data = search_school(query)
+  if not data: return jsonify({'res' : '검색결과가 없습니다.'})
+  return jsonify({'res' : [(i[1], i[2]) for i in data]})
+
+@app.route('/')
+def main():
+  return render_template('index.html')
